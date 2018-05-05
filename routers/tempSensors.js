983c73432ca2237b39287644874abec37b72cdb6
Christian Frenzl <chrisEff@users.noreply.github.com>
2018-05-05 22:12:52 +0200

    new endpoint for getting raw value from temperature sensor

--- routers/tempSensors.js
+++ routers/tempSensors.js
@@ -21,9 +21,18 @@ router.get('/:id', async (request, response, next) => {
 	next()
 })
 
+router.get('/:id/value', async (request, response, next) => {
+	const sensor = sensors.find(sensor => sensor.id === parseInt(request.params.id))
+	if (!sensor) {
+		return next(new errors.NotFoundError(`sensor ID ${request.params.id} not found`))
+	}
+	response.send(200, await readSensor(sensor.deviceId))
+	response.end()
+	next()
+})
+
 const readSensor = async (deviceId) => {
 	const lines = (await fs.readFile(`/sys/bus/w1/devices/${deviceId}/w1_slave`, 'utf8')).trim().split('\n')
-	console.log(lines)
 	if (lines.length === 2 && lines[0].endsWith(' YES')) {
 		const match = lines[1].match(/^(?:[0-9a-f]{2} )+t=([0-9]+)$/)
 		if (match) {
