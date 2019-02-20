b14be8cbfb6381cb0f726ae0f19346f1429d43f5
Christian Frenzl <chriseff@users.noreply.github.com>
2019-02-20 22:07:04 +0100

    increased test coverage

--- test/rf/RfController.test.js
+++ test/rf/RfController.test.js
@@ -1,5 +1,8 @@
 const sinon = require('sinon')
 const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+
+chai.use(chaiAsPromised)
 
 const OutletController = require('../../src/classes/rf/OutletController')
 
@@ -7,19 +10,40 @@ describe('OutletController', () => {
 
 	const sandbox = sinon.createSandbox()
 
-	const outletController = new OutletController([{
-		id: 123,
-		name: 'Outlet A',
-		off: 12345678,
-		on: 87654321,
-		protocol: 0,
-		pulseLength: 100,
-		state: 0,
-	}])
-
+	let outletController
 	let sendCodeStub, switchOutletSpy
+	let offStub, onStub
 
 	beforeEach(() => {
+		offStub = sandbox.stub()
+		onStub = sandbox.stub()
+
+		outletController = new OutletController([{
+			id: 123,
+			name: 'Outlet A',
+			off: 12345678,
+			on: 87654321,
+			protocol: 0,
+			pulseLength: 100,
+			state: 0,
+		}, {
+			id: 456,
+			name: 'Outlet B',
+			off: offStub,
+			on: onStub,
+			protocol: 0,
+			pulseLength: 100,
+			state: 0,
+		}, {
+			id: 789,
+			name: 'Outlet B',
+			off: 'invalid',
+			on: 'invalid',
+			protocol: 0,
+			pulseLength: 100,
+			state: 0,
+		}])
+
 		sendCodeStub = sandbox.stub(outletController, 'exec')
 		sendCodeStub.resolves('')
 
@@ -49,6 +73,24 @@ describe('OutletController', () => {
 
 			sinon.assert.calledOnce(sendCodeStub)
 			sinon.assert.calledWithExactly(sendCodeStub, `codesend 12345678 0 100`)
+		})
+
+		it('should execute OFF callback', async () => {
+			await outletController.switchOutlet(456, 0)
+
+			sinon.assert.calledOnce(offStub)
+			sinon.assert.notCalled(onStub)
+		})
+
+		it('should execute ON callback', async () => {
+			await outletController.switchOutlet(456, 1)
+
+			sinon.assert.calledOnce(onStub)
+			sinon.assert.notCalled(offStub)
+		})
+
+		it('should throw if "ON" is neither a code nor a callback', async () => {
+			return chai.assert.isRejected(outletController.switchOutlet(789, 1) )
 		})
 	})
 
