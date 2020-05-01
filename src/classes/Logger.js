6684d359da519b34bef5db28239834ecc8740d59
Christian Frenzl <chriseff@users.noreply.github.com>
2020-05-01 17:03:21 +0200

    initialize winston lazily

--- src/classes/Logger.js
+++ src/classes/Logger.js
@@ -6,71 +6,78 @@ const get = require('lodash.get')
 const winston = require('winston')
 const LogzioWinstonTransport = require('winston-logzio')
 
-const config = require('../../config.js')
+let winstonLogger
 
-const transports = []
+const getWinstonLogger = () => {
+	if (!winstonLogger) {
+		const config = require('../../config.js')
 
-if (get(config, 'logging.console.active')) {
-	const level = get(config, 'logging.console.level')
-	transports.push(new winston.transports.Console({
-		level,
-		timestamp: () => {
-			return dateFormat(new Date(), 'yyyy-mm-dd HH:MM:ss')
-		},
-	}))
+		const transports = []
+
+		if (get(config, 'logging.console.active')) {
+			const level = get(config, 'logging.console.level')
+			transports.push(new winston.transports.Console({
+				level,
+				timestamp: () => {
+					return dateFormat(new Date(), 'yyyy-mm-dd HH:MM:ss')
+				},
+			}))
+		}
+
+		if (get(config, 'logging.file.active')) {
+			const level = get(config, 'logging.file.level')
+			const filename = get(config, 'logging.file.filename')
+			transports.push(new winston.transports.File({
+				level,
+				filename,
+				timestamp: () => {
+					return dateFormat(new Date(), 'yyyy-mm-dd HH:MM:ss')
+				},
+			}))
+		}
+
+		if (get(config, 'logging.logzio.active')) {
+			const level = get(config, 'logging.logzio.level')
+			const token = get(config, 'logging.logzio.token')
+			transports.push(new LogzioWinstonTransport({
+				level,
+				name: 'winston_logzio',
+				token,
+				host: 'listener.logz.io',
+			}))
+		}
+
+		winstonLogger = winston.createLogger({
+			transports,
+		})
+	}
+	return winstonLogger
 }
-
-if (get(config, 'logging.file.active')) {
-	const level = get(config, 'logging.file.level')
-	const filename = get(config, 'logging.file.filename')
-	transports.push(new winston.transports.File({
-		level,
-		filename,
-		timestamp: () => {
-			return dateFormat(new Date(), 'yyyy-mm-dd HH:MM:ss')
-		},
-	}))
-}
-
-if (get(config, 'logging.logzio.active')) {
-	const level = get(config, 'logging.logzio.level')
-	const token = get(config, 'logging.logzio.token')
-	transports.push(new LogzioWinstonTransport({
-		level,
-		name: 'winston_logzio',
-		token,
-		host: 'listener.logz.io',
-	}))
-}
-
-const logger = winston.createLogger({
-	transports,
-})
 
 class Logger {
 
 	static error (msg, meta = undefined) {
-		logger.error(msg, meta)
+		getWinstonLogger().error(msg, meta)
 	}
 
 	static warn (msg, meta = undefined) {
-		logger.warn(msg, meta)
+		getWinstonLogger().warn(msg, meta)
 	}
 
 	static info (msg, meta = undefined) {
-		logger.info(msg, meta)
+		getWinstonLogger().info(msg, meta)
 	}
 
 	static verbose (msg, meta = undefined) {
-		logger.verbose(msg, meta)
+		getWinstonLogger().verbose(msg, meta)
 	}
 
 	static debug (msg, meta = undefined) {
-		logger.debug(msg, meta)
+		getWinstonLogger().debug(msg, meta)
 	}
 
 	static silly (msg, meta = undefined) {
-		logger.silly(msg, meta)
+		getWinstonLogger().silly(msg, meta)
 	}
 	
 }
